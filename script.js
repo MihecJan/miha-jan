ed504e26724a97b750400f939ceaf1fccc4356e4
Miha Jan <mihyslo@gmail.com>
2024-02-20 21:55:54 +0100

    Improve 3d avatar, adding projects...

--- script.js
+++ script.js
@@ -4,21 +4,58 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-window.onload = () => loadModel();
+let avatarContainer;
+let oldScrollY = window.scrollY;
+
+window.onload = () => {
+    avatarContainer = document.getElementById('avatar_container');
+    loadModel();
+    
+    const toggleView = document.querySelectorAll(".toggle-view-btn");
+    toggleView.forEach(btn => {
+        btn.addEventListener("click", e => handleProjectViewModeSwitch(e));
+    });
+};
+
+const initialAvatar = document.getElementById('initial_avatar');
+const testBalanceContainer = document.getElementById('test_balance_container');
+const progressContainer = document.querySelector('.progress-container');
+const progressBar = document.querySelector('.progress-container .bar-wrapper .bar');
+const progressText = document.querySelector('.progress-container .text');
+const progressPercentage = document.querySelector('.progress-container .percentage');
 
 const loadModel = () => {
     const loader = new GLTFLoader();
     loader.load('public/model_final.glb',
         (gltf) => {
-            // Loaded
+            // When 3d avatar is loaded, just displayed on the screen
+
+            // Hide initial avatar and progress bar.
+            initialAvatar.style.display = 'none';
+            progressContainer.style.display = 'none';
+            avatarContainer.classList.add('loaded');
+            avatarContainer.style.position = 'fixed';
+
+            /* Wait 100ms because the avatar was just inserted to the DOM,
+            so the opacity fade in works. */
+            setTimeout(() => { avatarContainer.style.opacity = '1'; }, 100);
+
+            // Show message and move a little to the side
+            setTimeout(() => { testBalanceContainer.classList.add('loaded'); }, 3000);
+            setTimeout(() => { avatarContainer.classList.add('move'); }, 3000);
             setupScene(gltf);
-            document.getElementById('avatar_loading').style.display = 'none';
         },
         (xhr) => {
             // Progress
             const percentCompletion = Math.round((xhr.loaded / xhr.total) * 100);
-            console.log(xhr.loaded, xhr.total);
-            document.getElementById('avatar_loading').innerText = `LOADING... ${percentCompletion}%`;
+            progressBar.style.width = `${percentCompletion}%`;
+            progressText.innerText = 'Loading avatar...';
+            progressPercentage.innerText = `${percentCompletion} %`;
+
+            if (percentCompletion === 100) {
+                initialAvatar.classList.add('percent-completion');
+                avatarContainer.classList.add('percent-completion');
+            }
         },
         (error) => {
             console.log(error);
@@ -33,16 +70,15 @@ const setupScene = gltf => {
     });
     renderer.outputColorSpace = THREE.SRGBColorSpace;
 
-    const container = document.getElementById('avatar_container');
-    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setSize(avatarContainer.clientWidth, avatarContainer.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-    container.appendChild(renderer.domElement);
+    avatarContainer.appendChild(renderer.domElement);
 
-    const camera = cameraSetup(container, renderer);
+    const camera = cameraSetup(avatarContainer, renderer);
 
     const scene = new THREE.Scene();
 
@@ -70,18 +106,24 @@ const setupScene = gltf => {
         'falling_to_landing': mixer.clipAction(fallingToLandingClip),
     };
 
-    const handleScrollAction = scrollIsDown => {
+    const handleScrollAction = scrollYDiff => {
+        // Hide text 'Test my balance by scrolling'
+        testBalanceContainer.classList.add('balance-tested');
+
         // If scrolling happens before the initial animation finishes.
         clearTimeout(defaultAnimation1);
         clearTimeout(defaultAnimation2);
 
-        if (scrollIsDown) {
+        // We are already inside throttled function and a fast scroll happened.
+        if (scrollYDiff < 0) {
+            // Scrolled down -> platform pushes avatar -> like elevator accelerating  up.
             currentActionStr = switchAction(actions, currentActionStr, 'hard_landing', 0.2);
             setTimeout(() => {
                 currentActionStr = switchAction(actions, currentActionStr, 'breathing_idle', 0.5);
             }, 1500);
         }
-        else {
+        else if (scrollYDiff > 0) {
+            // Scrolled up -> platform goes down -> like elevator accelerating down.
             currentActionStr = switchAction(actions, currentActionStr, 'falling_idle', 0.2);
             setTimeout(() => {
                 currentActionStr = switchAction(actions, currentActionStr, 'falling_to_landing', 0.2);
@@ -92,35 +134,52 @@ const setupScene = gltf => {
         }
     }
 
-    let last = 0;
-    let oldScrollY = window.scrollY;
+    // Time of last triggered avatar action (handleScrollAction)
+    let lastBalanceAction = 0;
+    let returnScrollTimeout;
     window.addEventListener('scroll', () => {
-        const scrollIsDown = (oldScrollY - window.scrollY) < 0;
+        // Get scroll difference between the current and last 'scroll' event.
+        const scrollYDiff = oldScrollY - window.scrollY
         oldScrollY = window.scrollY;
 
-        container.classList.remove('scrollReturn');
-        if (scrollIsDown) {
-            container.style.transform = "translateY(-50px)";
-            setTimeout(() => {
-                container.style.transform = "translateY(0)";
-                container.classList.add('scrollReturn');
-            }, 250);
+        // Move avatar to the side or back to center
+        if (window.scrollY > 80) {
+            avatarContainer.classList.add('to-side');
         }
         else {
-            container.style.transform = "translateY(50px)";
-            setTimeout(() => {
-                container.style.transform = "translateY(0)";
-                container.classList.add('scrollReturn');
-            }, 250);
+            avatarContainer.classList.remove('to-side');
         }
 
-        const now = new Date().getTime();
-        if (now - last < 1500) return;
-        last = now;
+        // Remove slow transform transition of the avatarContainer.
+        avatarContainer.classList.remove('scrollReturn');
 
-        handleScrollAction(scrollIsDown);
+        /* Limit movement to 100px, and move the platform to make it look realistic.
+        After timeout add the slow transform transition and move to original location.
+        Code inside setTimeout will only execute after there was no scroll for period of time.*/
+        const moveBy = (scrollYDiff * 5) < -100 ? -100
+            : (scrollYDiff * 5) > 100 ? 100
+            : scrollYDiff * 5;
+        avatarContainer.style.transform = "translate(70%, calc(-50% + " + moveBy + "px))";
+
+        clearTimeout(returnScrollTimeout);
+        returnScrollTimeout = setTimeout(() => {
+            avatarContainer.style.transform = "translate(70%, -50%)";
+            avatarContainer.classList.add('scrollReturn');
+        }, 400);
+
+        console.log(Math.abs(scrollYDiff));
+        /* If the scroll was strong, do something with the avatar.
+        Then do nothing to the avatar for 1500 ms (to finish the animation).*/
+        if (Math.abs(scrollYDiff) > 20) {
+            const now = new Date().getTime();
+            if (now - lastBalanceAction < 1500) return;
+            lastBalanceAction = now;
+
+            handleScrollAction(scrollYDiff);
+        }
     });
 
+    // This is the initial animation/action
     let currentActionStr = 'jumping_down'
     const clock = new THREE.Clock();
     const animate = () => {
@@ -133,6 +192,8 @@ const setupScene = gltf => {
     console.log('Action:', currentActionStr)
     actions[currentActionStr].play();
 
+    /* This is the default animation if the user is not interacting.
+    It will get cancelled with a strong scroll. (clearTimeout)*/
     const defaultAnimation1 = setTimeout(() => {
         currentActionStr = switchAction(actions, currentActionStr, 'waving', 1);
     }, 1700);
@@ -150,9 +211,9 @@ const switchAction = (actions, currentActionStr, toActionStr, duration) => {
     return toActionStr;
 }
 
-const cameraSetup = (container, renderer) => {
+const cameraSetup = (avatarContainer, renderer) => {
     const camera = new THREE.PerspectiveCamera(
-        45, container.clientWidth / container.clientHeight);
+        45, avatarContainer.clientWidth / avatarContainer.clientHeight);
     camera.position.set(0.2, 0.5, 1);
 
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -162,7 +223,7 @@ const cameraSetup = (container, renderer) => {
     controls.minDistance = 4;
     controls.minPolarAngle = 1.4;
     controls.maxPolarAngle = 1.4;
-    controls.target = new THREE.Vector3(0, 1, 0);
+    controls.target = new THREE.Vector3(0, 1, 0.25);
     controls.update();
 
     return camera;
@@ -180,11 +241,11 @@ const lightingSetup = scene => {
 
 const createPedestal = scene => {
     const groundGeometry = new THREE.CylinderGeometry(1, 1, 0.1, 5);
-    const groundMaterial = new THREE.MeshStandardMaterial();
+    const groundMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
     const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
     groundMesh.castShadow = false;
     groundMesh.receiveShadow = true;
-    groundMesh.position.y = -0.05;
+    groundMesh.position.y = -0.1;
     scene.add(groundMesh);
 }
 
@@ -199,14 +260,4 @@ const loadAvatar = (gltf, scene) => {
     scene.add(avatar);
 
     return avatar;
-}
-
-const throttle = (func, delay) => {
-    let last = 0;
-    return (...args) => {
-        const now = new Date().getTime();
-        if (now - last < delay) return;
-        last = now;
-        func(...args);
-    }
 }
